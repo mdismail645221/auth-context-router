8d8d47df5ba06f85c38027e7f410ab8efe561fec
mdismail645221 <mdismail645221@gmail.com>
2022-10-16 21:52:31 +0600

    auth context router

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { NavLink  } from "react-router-dom";
+import { AuthContext } from '../contexts/UserContext';
+
+const Header = () => {
+    
+    const {user} = useContext(AuthContext);
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Auth context router</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                    <li><NavLink  to='/home'>Home</NavLink ></li>
+                    <li><NavLink  to='/register'>Register</NavLink ></li>
+                    <li><NavLink to='/login'>Login</NavLink ></li>
+                    {user?.displayName && <li><span>Welcome, {user.displayName}</span></li>}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxPwh4BYEQlOWlkOxGYfBYQWfIzy4wxg4",
+  authDomain: "auth-router-context-b194f.firebaseapp.com",
+  projectId: "auth-router-context-b194f",
+  storageBucket: "auth-router-context-b194f.appspot.com",
+  messagingSenderId: "977926882363",
+  appId: "1:977926882363:web:6fbb849dd1096fa3ae481e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
